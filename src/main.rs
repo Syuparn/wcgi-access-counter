c27ede39f18ac380d5cf1dfb389dcd5a858542a3
syuparn <s.hello.spagetti@gmail.com>
2023-04-25 08:54:02 +0900

    cosmetic change

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use cgi::{http::StatusCode, Request, Response};
 mod counter;
 
+const COUNTER_FILE_PATH: &str = "/tmp/counter/counter.txt";
+
 fn main() {
     cgi::handle(handler);
 }
 
 fn handler(_request: Request) -> Response {
-    // TODO: handle error
-    let file_path = "/tmp/counter/counter.txt";
-    let pageviews = match counter::increment_counter(file_path) {
+    let pageviews = match counter::increment_counter(COUNTER_FILE_PATH) {
         Ok(pageviews) => pageviews,
         Err(e) => {
             eprintln!("error: {e}");
-            return cgi::string_response(StatusCode::INTERNAL_SERVER_ERROR, format!("{{\"error\": \"{e}\"}}"))
+            return cgi::string_response(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("{{\"error\": \"{e}\"}}"),
+            );
         }
     };
 
